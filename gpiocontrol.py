71f17c750745c2acb8ebf39642fd7f55771ffee2
kaiongit <60166885+kaiongit@users.noreply.github.com>
2021-08-26 20:01:52 +0800

    Create gpiocontrol.py

---
+++ gpiocontrol.py
@@ -0,0 +1,53 @@
+import constants
+import RPi.GPIO as GPIO
+from RpiMotorLib import RpiMotorLib
+# 156 steps for 3.2 cm?
+
+# Lower level control
+def setup():
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setwarnings(False)
+
+def setup_output_pin(pin: int):
+    setup()
+    GPIO.setmode(pin, GPIO.OUT)
+
+def setup_input_pin(pin: int):
+    setup()
+    GPIO.setmode(pin, GPIO.IN)
+
+def set_pin_high(pin: int):
+    setup_output_pin(pin)
+    GPIO.output(pin, GPIO.HIGH)
+
+def set_pin_low(pin: int):
+    setup_output_pin(pin)
+    GPIO.output(pin, GPIO.LOW)
+
+def get_pin_level(pin: int) -> int:
+    setup_input_pin(pin)
+    return GPIO.input(pin)
+
+def move_stepper_motor(motor: list, distance_cm: float):
+    RpiMotorLib.BYJMotor().motor_run(motor, (distance_cm * constants.HALF_STEPS_TO_1CM))
+
+# Higher level control
+turn_on_ringlight = set_pin_high(constants.LEDPIN)
+
+turn_off_ringlight = set_pin_low(constants.LEDPIN)
+
+signal_start_cleaning = set_pin_high(constants.START_CLEANING_PIN)
+
+signal_stop_cleaning = set_pin_high(constants.STOP_PIN)
+
+def cleanup_signal_pins():
+    set_pin_low(constants.START_CLEANING_PIN)
+    set_pin_low(constants.STOP_PIN)
+
+get_signal_done_cleaning = get_pin_level(constants.DONE_CLEANING_PIN)
+
+def goto_next_grid():
+    move_stepper_motor(constants.MOTOR2, constants.GRID_SIDE_LENGTH)
+
+
+# TODO: how to make the stop signal and the listening bit?
