8153c5418172dff686c2d2a50fe6399f5c22a428
Wilbur <wilburlua910@outlook.com>
2021-08-26 15:53:00 +0800

    Update lcd-screen.py
    Fill up logic for camera, pins and GUI stuff

--- lcd-screen.py
+++ lcd-screen.py
@@ -7,11 +7,6 @@ import time
 from enum import Enum
 from guizero import App, Text, PushButton, Box
 
-class State(Enum):
-    WAITING = 0
-    START_CLEANING = 1
-    DONE_CLEANING = 2
-
 positive_green = "#6eff56"
 negative_red = "#ff3333"
 confused_yellow = "#fcff56"
@@ -19,41 +14,21 @@ confused_yellow = "#fcff56"
 # App stuff
 os.environ["DISPLAY"] = ":0.0"
 
-state_queue = queue.Queue()
-
-def queue_listener():
-    while 1:
-        for state in list(state_queue.queue):
-            print(state)
-
-
-# threading.Thread(target=queue_listener).start()
-
 # Screen stuff
 def goto_cleaning_screen():
     start_screen_box.hide()
     cleaning_screen_box.show()
-    state_queue.put(State.START_CLEANING)
-
-    # for demo
-    threading.Thread(target=arduino_ack, args=(state_queue, )).start()
-
-# for demo
-def arduino_ack(state_queue):
-    time.sleep(3)
-    state_queue.get(timeout=1)
-    state_queue.put(State.DONE_CLEANING)
 
 previous_screen: Box = None
 # previous_repeat = None
 
 def goto_confirm_stop_screen(previous_screen1):
     global previous_screen
-    # global previous_repeat
+
     previous_screen = previous_screen1
-    # previous_repeat = previous_screen1.repeat
+
     previous_screen.hide()
-    # previous_screen.cancel()
+
     confirm_stop_screen_box.show()
 
 def goback():
@@ -68,25 +43,11 @@ def goto_start_screen():
     complete_bad_screen_box.hide()
     start_screen_box.show()
 
-# not used in command params, executed by worker threads/listeners
-def goto_drying_screen():
-    cleaning_screen_box.hide()
-    drying_screen_box.show()
-    
-    # for demo
-    threading.Thread(target=zoom).start()
-
-
-# for demo
-def zoom():
-    time.sleep(3)
-    goto_inspecting_screen()
-    time.sleep(3)
-    goto_complete_good_screen()
-
 def goto_inspecting_screen():
-    drying_screen_box.hide()
+    cleaning_screen_box.hide()
     inspecting_screen_box.show()
+
+    #TODO - Camera stuff
 
 def goto_complete_good_screen():
     inspecting_screen_box.hide()
@@ -94,9 +55,15 @@ def goto_complete_good_screen():
 
 # poll based listeners
 def cleaning_done():
-    if cleaning_screen_box.visible and state_queue.qsize() > 0 and state_queue.queue[0] == State.DONE_CLEANING:
-        state_queue.get()
-        goto_drying_screen()
+    if cleaning_screen_box.visible:
+
+        #TODO - Check if Arduino pin is HIGH 
+        goto_inspecting_screen()
+
+def inspecting_done():
+    pass
+    #TODO - check boolean input from camera 
+    #Check if -> good ? 1 , bad? 2, not done yet ? 0
 
 # Create app object
 app = App(title="SyClone")
@@ -110,32 +77,21 @@ ssb_titletext = Text(start_screen_box, text="SyClone", color="white")
 ssb_start_button = PushButton(start_screen_box, text="Start", command=goto_cleaning_screen, width="fill", height="fill")
 ssb_start_button.bg = positive_green
 
-# Restart 
-# restart_screen_box = Box(app, visible=False, width="fill", height="fill")
-# rsb_title_text = Text(restart_screen_box, text="Restart from previously stopped cycle?", color="white")
-# rsb_cancel_button = PushButton(restart_screen_box, text="Continue washing", command=goto_start_screen, width="fill", height="fill")
-# rsb_cancel_button.bg = positive_green
-# rsb_confirm_button = PushButton(restart_screen_box, text="Stop", command=goto_start_screen, width="fill", height="fill")
-# rsb_confirm_button.bg = negative_red
+#TODO: gotocleaningscreen => pull pin to high to indicate standy > start 
 
 # Cleaning screen
 cleaning_screen_box = Box(app, visible=False, width="fill", height="fill")
-csb_title_text = Text(cleaning_screen_box, text="Washing and sterilising", color="white")
+csb_title_text = Text(cleaning_screen_box, text="Cleaning", color="white")
 csb_stop_button = PushButton(cleaning_screen_box, text="Stop", command=goto_confirm_stop_screen, args=[cleaning_screen_box], width="fill", height="fill")
 csb_stop_button.bg = negative_red
 cleaning_screen_box.repeat(1000, cleaning_done)
-
-# Drying
-drying_screen_box = Box(app, visible=False, width="fill", height="fill")
-dsb_title_text = Text(drying_screen_box, text="Drying", color="white")
-dsb_stop_button = PushButton(drying_screen_box, text="Stop", command=goto_confirm_stop_screen, args=[drying_screen_box], width="fill", height="fill")
-dsb_stop_button.bg = negative_red
 
 # Inspecting
 inspecting_screen_box = Box(app, visible=False, width="fill", height="fill")
 isb_title_text = Text(inspecting_screen_box, text="Inspecting", color="white")
 isb_stop_button = PushButton(inspecting_screen_box, text="Stop", command=goto_confirm_stop_screen, args=[inspecting_screen_box], width="fill", height="fill")
 isb_stop_button.bg = negative_red
+inspecting_screen_box.repeat(1000, inspecting_done)
 
 # Confirm stop screen
 confirm_stop_screen_box = Box(app, visible=False, width="fill", height="fill")
